740ca5328688290228702a7510741fe6ae48b8a2
blueberry1310 <jiwoni1310@gmail.com>
2024-07-08 12:38:50 +0900

    Update13 script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const display = document.getElementById('display');
     const keys = document.querySelectorAll('.key');
     const deleteBtn = document.getElementById('delete');
-    const clearBtn = document.getElementById('clear');
     const enterBtn = document.getElementById('enter');
 
     keys.forEach(key => {
@@ -13,10 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     deleteBtn.addEventListener('click', () => {
         display.value = display.value.slice(0, -1);
-    });
-
-    clearBtn.addEventListener('click', () => {
-        display.value = '';
     });
 
     enterBtn.addEventListener('click', () => {
